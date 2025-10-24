20a19e16be364899da027c5f2e0c14693d273260
Sebastian Schnichels <seb0305@gmx.de>
2025-10-24 13:50:02 +0200

    Integrated Dotenv

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,7 +1,12 @@
 import requests
+import os
+from dotenv import load_dotenv
 from typing import Any, Dict, List
 
-API_KEY = "yMFgesWf8cTpujXTRlKTfg==P7czWNYAwh7rLA6J"
+load_dotenv()  # Load variables from .env into environment
+
+API_KEY = os.getenv('API_KEY')
+print(API_KEY)  # Optional: print to verify it was loaded correctly
 API_URL = "https://api.api-ninjas.com/v1/animals"
 
 def fetch_data(animal_name: str) -> List[Dict[str, Any]]:
